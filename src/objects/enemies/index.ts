a9799fd78b92df0b7dd9f32207469246b2c430a1
ishihara1989 <ashitahamatsuri@gmail.com>
2025-03-06 17:39:34 +0900

    Add enemy factory implementation

---
+++ src/objects/enemies/index.ts
@@ -0,0 +1,38 @@
+import { OrcEnemy } from './OrcEnemy';
+import { SlimeEnemy } from './SlimeEnemy';
+import { GoblinEnemy } from './GoblinEnemy';
+import { BattleScene } from '../../scenes/BattleScene';
+
+// エネミータイプの列挙型
+export enum EnemyType {
+  GOBLIN = 'goblin',
+  ORC = 'orc',
+  SLIME = 'slime'
+}
+
+// エネミー生成のファクトリークラス
+export class EnemyFactory {
+  // エネミーを生成するファクトリーメソッド
+  static createEnemy(
+    type: EnemyType,
+    scene: BattleScene,
+    x: number,
+    y: number,
+    level: number = 1
+  ) {
+    switch (type) {
+      case EnemyType.GOBLIN:
+        return new GoblinEnemy({ scene, x, y, level });
+      case EnemyType.ORC:
+        return new OrcEnemy({ scene, x, y, level });
+      case EnemyType.SLIME:
+        return new SlimeEnemy({ scene, x, y, level });
+      default:
+        // デフォルトはゴブリン
+        return new GoblinEnemy({ scene, x, y, level });
+    }
+  }
+}
+
+// エクスポートするクラス
+export { OrcEnemy, SlimeEnemy, GoblinEnemy };
