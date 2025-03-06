ad5a9ce9c8da1fdd03234869804f34f97ed12951
ishihara1989 <ashitahamatsuri@gmail.com>
2025-03-06 20:30:06 +0900

    Simplify ESLint config using Flat Config format

--- eslint.config.js
+++ eslint.config.js
@@ -1,33 +1,32 @@
 // eslint.config.js
 const tseslint = require('@typescript-eslint/eslint-plugin');
 const tsparser = require('@typescript-eslint/parser');
-const prettierConfig = require('eslint-config-prettier');
 const prettierPlugin = require('eslint-plugin-prettier');
 
+// 設定を簡略化
 module.exports = [
   {
-    ignores: ['node_modules', 'dist', 'webpack.config.js']
+    // グローバル設定
+    ignores: ['node_modules/**', 'dist/**', 'webpack.config.js'],
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: 'module',
+      globals: {
+        // ブラウザとES2021のグローバル変数
+        window: 'readonly',
+        document: 'readonly',
+        navigator: 'readonly'
+      }
+    }
   },
+  // TypeScript ファイル用の設定
   {
-    files: ['**/*.{js,ts}'],
+    files: ['**/*.ts'],
     languageOptions: {
       parser: tsparser,
       parserOptions: {
-        ecmaVersion: 2021,
-        sourceType: 'module',
         project: './tsconfig.json'
-      },
-      // 環境設定を直接指定する
-      globals: {
-        // ブラウザのグローバル変数
-        window: 'readonly',
-        document: 'readonly',
-        navigator: 'readonly',
-        // ES2021のグローバル変数
-        Promise: 'readonly',
-        Map: 'readonly',
-        Set: 'readonly'
-      },
+      }
     },
     plugins: {
       '@typescript-eslint': tseslint,
@@ -39,16 +38,17 @@ module.exports = [
       '@typescript-eslint/no-explicit-any': 'warn',
       '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
       'no-console': ['warn', { allow: ['warn', 'error'] }]
-    },
-    // ESLint v9の環境設定方法
-    linterOptions: {
-      reportUnusedDisableDirectives: true,
-    },
-    // 環境設定
-    env: {
-      browser: true,
-      es2021: true
     }
   },
-  prettierConfig
+  // JavaScript ファイル用の設定
+  {
+    files: ['**/*.js'],
+    plugins: {
+      prettier: prettierPlugin
+    },
+    rules: {
+      'prettier/prettier': 'error',
+      'no-console': ['warn', { allow: ['warn', 'error'] }]
+    }
+  }
 ];
